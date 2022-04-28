df1f7f2b65afa5a7a9ab55f751e3d8dacd7f7d0a
Shivani Vishwakarma <38506074+ShivaniVishwakarma@users.noreply.github.com>
2022-04-28 21:27:38 +0200

    Add files via upload

---
+++ src/main/java/com/demoprojects/department/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.demoprojects.department.controller;
+
+import com.demoprojects.department.entity.Department;
+import com.demoprojects.department.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/departments")
+@Slf4j
+public class DepartmentController {
+
+    @Autowired
+    DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department){
+       log.info("Inside saveDepartment method of DepartmentController");
+       return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findDepartmentById(@PathVariable("id") Long departmentId){
+        log.info("Inside findDepartmentById method of DepartmentController");
+        return departmentService.findDepartmentById(departmentId);
+    }
+}
